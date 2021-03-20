f482a99045b85714370bdcfdc98854ab79349f2d
jrudio <justinjrudio@gmail.com>
2021-03-20 13:11:02 -0700

    render the title service (Chloe) and description service (Justin)

--- server/index.js
+++ server/index.js
@@ -7,18 +7,23 @@ const staticFilesPath = path.resolve(__dirname, '../client');
 
 // TODO: utilize this in proxyRequest
 const proxyHosts = {
-  chloeTitleService: process.env.CHLOE_TITLE_HOST || '127.0.0.1',
+  chloeTitleService: {
+    host: process.env.CHLOE_TITLE_HOST || '127.0.0.1',
+    path: '/bundle.js',
+    port: process.env.CHLOE_TITLE_HOST || 3000
+  },
   justinDescriptionService: {
     host: process.env.JUSTIN_DESCRIPTION_HOST || '127.0.0.1',
+    path: '/index.js',
     port: process.env.JUSTIN_DESCRIPTION_PORT || 7878
   }
 };
 
-const proxyRequest = (host, port, path) => {
+const proxyRequest = ({ host, port, path }) => {
   return {
     port,
     path,
-    host: '127.0.0.1',
+    host,
     method: 'GET',
     headers: {}
   };
@@ -29,13 +34,8 @@ app.use('/', express.static(staticFilesPath));
 app.get('/chloe-title-service', (req, res) => {
   console.log('fetching chloe\'s bundle.js');
 
-  res.status(200).send();
-});
-
-app.get('/justin-description-service', (req, res) => {
-  console.log('fetching Justin\'s bundle.js');
-
-  const options = proxyRequest(proxyHosts.justinDescriptionService, '/index.js', 7878);
+  // TODO: refactor proxy requests
+  const options = proxyRequest(proxyHosts.chloeTitleService);
 
   const proxyConn = http.request(options, (proxyRes) => {
     res.writeHead(proxyRes.statusCode);
@@ -45,7 +45,63 @@ app.get('/justin-description-service', (req, res) => {
     proxyRes.on('close', data => res.end());
     proxyRes.on('end', data => res.end());
   }).on('error', err => {
-    console.log(`failed to proxy request to ${proxyHost.justinDescriptionService}:${err.message}`);
+    console.log(`failed to proxy request to ${proxyHosts.chloeTitleService}:${err.message}`);
+  });
+
+  proxyConn.end();
+});
+
+// chloe's service goes through this route
+app.get('/title/:id', (req, res) => {
+  console.log('proxying to chloe\'s service');
+
+  const { id } = req.params;
+
+  console.log(`fetching title with id: ${id}`);
+
+  const {
+    host,
+    port
+  } = proxyHosts.chloeTitleService;
+
+  const proxyPath = `/api/${req.url}`;
+
+  console.log(`proxying request to: ${proxyPath}`);
+
+  const options = proxyRequest({
+    host,
+    port,
+    path: proxyPath
+  });
+
+  const proxyConn = http.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode);
+    proxyRes.setEncoding('utf8');
+
+    proxyRes.on('data', data => res.write(data));
+    proxyRes.on('close', data => res.end());
+    proxyRes.on('end', data => res.end());
+  }).on('error', err => {
+    console.log(`failed to proxy request to ${proxyHosts.chloeTitleService}:${err.message}`);
+  });
+
+  proxyConn.end();
+});
+
+app.get('/justin-description-service', (req, res) => {
+  console.log('fetching Justin\'s bundle.js');
+
+  const options = proxyRequest(proxyHosts.justinDescriptionService);
+
+  const proxyConn = http.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode);
+    proxyRes.setEncoding('utf8');
+
+    proxyRes.on('data', data => res.write(data));
+    proxyRes.on('close', data => res.end());
+    proxyRes.on('end', data => res.end());
+  }).on('error', err => {
+    console.log(`failed to proxy request to ${proxyHosts.justinDescriptionService}:${err.message}`);
   });
 
   proxyConn.end();
